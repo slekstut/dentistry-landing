59de167adbeb133ebf1fa34976ff24f86f868922
Sarunas <sarunas.lekstutis@gmail.com>
2021-05-19 22:45:54 +0300

    last chapter started

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Main />
     <Chapter3 />
+    <Chapter6 />
   </div>
 </template>
 
 <script>
 import Main from "./components/Main.vue";
 import Chapter3 from "./components/Chapter3.vue";
+import Chapter6 from "./components/Chapter6.vue";
 
 export default {
   name: "App",
   components: {
     Main,
     Chapter3,
+    Chapter6,
   },
 };
 </script>
---
+++ src/components/Chapter6.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="chapter6">
+      <div class="text-wrapper">
+        <h1 class="chapter">06</h1>
+        <h5>Pradėti yra paprasta</h5>
+        <h3>ženkite pirmą žingsnį</h3>
+        <h4>Registracija nemokamai konsultacijai</h4>
+      </div>
+      <p>Liko <span>9</span> nemokamos konsultacijos</p>
+      <div class="arrow"></div>
+      <form>
+        <input class="name__input" type="text" placeholder="Vardas, pavardė" />
+        <input class="mobile__input" type="number" placeholder="Telefonas" />
+        <input class="submit__input" type="submit" value="REGISTRUOTIS" />
+      </form>
+    </div>
+    <div class="map"></div>
+    <div class="footer-curve"></div>
+  </div>
+</template>
