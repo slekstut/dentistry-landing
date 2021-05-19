9588660ee15ed1dcd914a44b0cc6b7cd68e75824
Sarunas <sarunas.lekstutis@gmail.com>
2021-05-19 12:33:27 +0300

    1st chapter done

---
+++ src/components/Main.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper-content">
+    <section class="wrapper-main">
+      <div class="logo">
+        <img src="/assets/img/logo.png" alt="logo">
+      </div>
+      <div class="main-title">
+        <h3>Neleiskite, kad prarasti dantys</h3>
+        <h1>taptų nuosprendžiu!</h1>
+        <h5>Kodėl netenkame dantų?</h5>
+        <p class="chapter">01</p>
+      </div>
+      <div class="second-half">
+        <div class="card">
+          <img src="/assets/img/1.jpg" alt="periodonto-ligos" />
+          <p>Periodonto ligos</p>
+        </div>
+        <div class="card">
+          <img src="/assets/img/2n-1.jpg" alt="eduonis" />
+          <p>Ėduonies ir kiti danties pažeidimai</p>
+        </div>
+        <div class="card">
+          <img src="/assets/img/3n-1.jpg" alt="fizines-traumos" />
+          <p>Fizinės traumos</p>
+        </div>
+        
+      </div>
+      <div class="hero-text">
+          <p>Klaidingas įsitikinimas, esą dantų netenka tik vyresnio amžiaus žmonės. Iš tiesų vieną ar daugiau dantų yra praradęs kas antras vyresnis nei 25 metų lietuvis! Dažna dantų netekimo priežastis - bet kokios amžiaus pacientams gresiančios periodonto ligos, ypač giliųjų burnos audinių infekcija periodontitas, kuri lemia žandikaulio kaulo nykimą.</p>
+          <p>Į dantis kėsinasi ir nepastebimai prasidėjęs ėduonis: neaptiktas šis lėtinis susirgimas paveikia vidinius danties audinius ir gali lemti danties netekimą.</p>
+          <p>Kita dažna dantų praradimo priežastis, kurios aukomis neretai tampa aktyviai sportuojantys žmonės - įvairios dantų traumos.</p>
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+// @import '../assets/scss/styles.scss';
+
+// @import '@/assets/scss/styles.scss';
+</style>
