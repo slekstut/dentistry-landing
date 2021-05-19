a22aec39f2f034506e1d6e4ce1c025d265c72831
Sarunas <sarunas.lekstutis@gmail.com>
2021-05-19 16:21:11 +0300

    3rd chapter completed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Main />
+    <Chapter3 />
   </div>
 </template>
 
 <script>
 import Main from "./components/Main.vue";
+import Chapter3 from "./components/Chapter3.vue";
 
 export default {
   name: "App",
   components: {
     Main,
+    Chapter3,
   },
 };
 </script>
---
+++ src/components/Chapter3.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="chapter3">
+   <div class="background-curve"></div>
+    <div>
+      <h1 class="chapter">03</h1>
+      <h3>Gydytojai pataria</h3>
+    </div>
+    <div class="doctors-content">
+      <div class="doctor-card">
+        <img src="/assets/img/9.png" alt="doctor-lior" />
+        <div class="doctor-text">
+          <h5>Lior Nachmaney</h5>
+          <p>Gyd. odontologas - burnos chirurgas</p>
+          <p>
+            „Pacientai susiduria su dilema - rinktis išimamus protezus ar dantų
+            implantus. Nors protezai yra pigesnis sprendimas, nerekomenduoju:
+            jie atkuria tik 10 proc. kramtymo funkcijos, prastai fiksuojasi, yra
+            nepatogūs ir greitai dėvisi, todėl juos tenka gana dažnai keisti.
+            Tuo tarpu dantų implantai džiugina natūralių dantų pojūčiu, puikiai
+            atkuria kramtymo funkciją ir nepriekaištingą estetiką."
+          </p>
+        </div>
+        </div>
+
+        <div class="doctor-card">
+          <img src="/assets/img/8.png" alt="doctor-monika" />
+          <div class="doctor-text">
+            <h5>Monika Kulpavičiūtė</h5>
+            <p>Gyd. odontologė - veido ir žandikaulių chirurgė</p>
+            <p>
+              „Renkantis dantų atkūrimą implantais, patariu atkreipti dėmesį į
+              tai, iš kokios medžiagos gaminami dantų vainikėliai ar dantų
+              plokštelės. Pacientams, vertinantiems estetiką ir ilgalaikį
+              rezultatą, rekomenduoju rinktis cirkonio oksidą - tai
+              patvariausia, estetiškiausia ir jokios alergijos nesukelianti
+              medžiaga, kuri suteiks natūralumą Jūsų šypsenai."
+            </p>
+          </div>
+      </div>
+    </div>
+    <div class="background-curve2"></div>
+  </section>
+</template>
