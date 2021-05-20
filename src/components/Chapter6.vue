b5f2d9e36540683925f56f0b31bb1ebb6fb465f0
Sarunas <sarunas.lekstutis@gmail.com>
2021-05-20 10:49:45 +0300

    6th chapter done

--- src/components/Chapter6.vue
+++ src/components/Chapter6.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="chapter6-wrapper">
     <div class="chapter6">
+      <div class="chapter6-curve"></div>
       <div class="text-wrapper">
         <h1 class="chapter">06</h1>
         <h5>Pradėti yra paprasta</h5>
@@ -8,14 +9,43 @@
         <h4>Registracija nemokamai konsultacijai</h4>
       </div>
       <p>Liko <span>9</span> nemokamos konsultacijos</p>
-      <div class="arrow"></div>
+      <div class="arrow">
+        <img src="/assets/svg/KreivesAsset8.svg" alt="" />
+      </div>
       <form>
         <input class="name__input" type="text" placeholder="Vardas, pavardė" />
         <input class="mobile__input" type="number" placeholder="Telefonas" />
         <input class="submit__input" type="submit" value="REGISTRUOTIS" />
       </form>
     </div>
-    <div class="map"></div>
-    <div class="footer-curve"></div>
+    <div class="footer-wrapper">
+      <div class="footer-content">
+        <img src="/assets/img/logo.png" alt="logo" />
+        <div class="address-details">
+          <h5>Mus rasite</h5>
+          <p>Molėtų g. 13-1A, Didžioji Riešė,</p>
+          <p>LT-14264 Vilniaus raj.</p>
+          <p>Nemokamas automobilių stovėjimas</p>
+        </div>
+        <div class="working-hours">
+          <h5>Darbo laikas</h5>
+          <p>I-V - 10:00-19:00,</p>
+          <p>VI - 10:00-16:00</p>
+          <span
+            ><a href="#"
+              ><img src="/assets/svg/facebook-f-brands.svg" alt="" /></a
+          ></span>
+          <span
+            ><a href="#"
+              ><img class="insta" src="/assets/svg/instagram-brands.svg" alt="" /></a
+          ></span>
+        </div>
+      </div>
+    <div class="footer-bckgr">
+      <div class="map"></div>
+      <div class="footer-curve"></div>
+    </div>
+    </div>
+
   </div>
 </template>
